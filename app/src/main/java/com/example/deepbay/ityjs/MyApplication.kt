11976bca12e656565434462198d4aea5739b9cc7
lwd <wendongli@deepbaytech.com>
2018-10-24 15:41:31 +0800

    kotlin+mvp creat

---
+++ app/src/main/java/com/example/deepbay/ityjs/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.deepbay.ityjs
+
+
+/**
+ * @author     IT烟酒僧
+ * created   2018/10/24 14:24
+ * desc:
+ */
---
+++ app/src/main/java/com/example/deepbay/ityjs/base/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.example.deepbay.ityjs.base
+
+
+/**
+ * @author     IT烟酒僧
+ * created   2018/10/24 12:24
+ * desc:
+ */
