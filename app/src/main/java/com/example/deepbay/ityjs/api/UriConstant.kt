e5d1961daedaf91516889a1e7d9ad536d74cc432
lwd <wendongli@deepbaytech.com>
2018-11-09 17:18:13 +0800

    api和bean类实现

---
+++ app/src/main/java/com/example/deepbay/ityjs/api/UriConstant.kt
@@ -0,0 +1,11 @@
+package com.example.deepbay.ityjs.api
+
+
+/**
+ * @author     IT烟酒僧
+ * created   2018/11/9 17:13
+ * desc:
+ */
+object UriConstant{
+    const val BASE_URL="http://baobab.kaiyanapp.com/api/"
+}
