e7002e377576ead87084c6de87fd29ef3e095def
lwd <wendongli@deepbaytech.com>
2018-11-28 17:03:17 +0800

    homeFragment逻辑实现

--- app/src/main/java/com/example/deepbay/ityjs/ui/adapter/HomeAdapter.kt
+++ app/src/main/java/com/example/deepbay/ityjs/ui/adapter/HomeAdapter.kt
@@ -12,6 +12,61 @@ import com.example.deepbay.ityjs.view.recyclerview.adapter.CommonAdapter
  * desc:
  */
 class HomeAdapter(context: Context,data:ArrayList<HomeBean.Issue.Item>):CommonAdapter<HomeBean.Issue.Item>(context,data,-1){
+
+
+
+    //banner作为recyclerview的第一项
+
+    var bannerItemSize=0
+
+    companion object {
+        private const val ITEM_TYPE_BANNER=1
+        private const val ITEM_TYPE_TEXT_HEADER=2
+        private const val ITEM_TYPE_CONTENT=3
+
+    }
+
+    /**
+     * 设置banner大小
+     */
+
+    fun setBannerSize(count:Int){
+        bannerItemSize=count
+    }
+
+    /**
+     * 添加更多数据
+     */
+
+    fun addItemData(itemList:ArrayList<HomeBean.Issue.Item>){
+        this.mData.addAll(itemList)
+        notifyDataSetChanged()
+    }
+
+    /**
+     * 得到item类型
+     */
+    override fun getItemViewType(position: Int): Int {
+
+        return when {
+            position == 0 ->
+                ITEM_TYPE_BANNER
+            mData[position + bannerItemSize - 1].type == "textHeader" ->
+                ITEM_TYPE_TEXT_HEADER
+            else ->
+                ITEM_TYPE_CONTENT
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return when{
+            mData.size>bannerItemSize->mData.size-bannerItemSize+1
+            mData.isEmpty()->0
+            else->1
+        }
+    }
+
+
     override fun bindData(holder: ViewHolder, data: HomeBean.Issue.Item, position: Int) {
 
     }
--- app/src/main/java/com/example/deepbay/ityjs/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/deepbay/ityjs/ui/fragment/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.example.deepbay.ityjs.ui.fragment
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.example.deepbay.ityjs.R
 import com.example.deepbay.ityjs.base.BaseFragment
 import com.example.deepbay.ityjs.mvp.contract.HomeContract
@@ -11,6 +12,7 @@ import com.example.deepbay.ityjs.mvp.presenter.HomePresenter
 import com.example.deepbay.ityjs.net.exception.ErrorStatus
 import com.example.deepbay.ityjs.showToast
 import com.example.deepbay.ityjs.ui.adapter.HomeAdapter
+import com.hazz.kotlinmvp.utils.StatusBarUtil
 import com.orhanobut.logger.Logger
 import com.scwang.smartrefresh.header.MaterialHeader
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -41,6 +43,7 @@ class HomeFragment:BaseFragment(),HomeContract.View{
             val fragment=HomeFragment()
             val bundle=Bundle()
             fragment.arguments=bundle
+            fragment.mTitle=title
             return fragment;
         }
     }
@@ -59,6 +62,62 @@ class HomeFragment:BaseFragment(),HomeContract.View{
         //此处的this 代表view:Ibaseview
         //initView 给fragment设置数据,设置完后绑定
         mPresenter.attachView(this)
+        mRefreshLayout.setEnableHeaderTranslationContent(true)
+        mRefreshLayout.setOnRefreshListener{
+            isRefresh=true
+            mPresenter.requestHomeData(num)
+        }
+
+        mMaterialHeader=mRefreshLayout.refreshHeader as MaterialHeader?
+        //打开下拉刷新区域块背景
+        mMaterialHeader?.setShowBezierWave(true)
+        //设置下拉刷新主体颜色
+
+        mRecyclerView.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState==RecyclerView.SCROLL_STATE_IDLE){
+                    val childCount=mRecyclerView.childCount
+                    val itemCount=mRecyclerView.layoutManager.itemCount
+                    val firstVisibleItem=(mRecyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                    if (firstVisibleItem+childCount==itemCount){
+                        if (!loadingMore){
+                            loadingMore=true
+                            mPresenter.loadMoreData()
+                        }
+                    }
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                val currentVisibleItemPosition=linearLayoutManager.findFirstVisibleItemPosition()
+                if (currentVisibleItemPosition==0){
+                    //背景设置为透明
+                    toolbar.setBackgroundColor(getColor(R.color.color_translucent))
+                    iv_search.setImageResource(R.mipmap.ic_action_search_white)
+                    tv_header_title.text=""
+                }else{
+                    if (mHomeAdapter?.mData!!.size>1){
+                        toolbar.setBackgroundColor(getColor(R.color.color_title_bg))
+                        iv_search.setImageResource(R.mipmap.ic_action_search_black)
+                        val itemList=mHomeAdapter!!.mData
+                        val item=itemList[currentVisibleItemPosition+mHomeAdapter!!.bannerItemSize-1]
+                        if (item.type=="textHeader"){
+                            tv_header_title.text=item.data?.text
+                        }else{
+                            tv_header_title.text=simpleDateFormat.format(item.data?.date)
+                        }
+                    }
+                }
+            }
+        })
+
+        //iv_search.setOnClickListener()
+        mLayoutStatusView=multipleStatusView
+        //状态栏透明和间距处理
+        activity?.let { StatusBarUtil.darkMode(it) }
+        activity?.let { StatusBarUtil.setPaddingSmart(it,toolbar) }
     }
 
     override fun lazyLoad() {
@@ -83,8 +142,8 @@ class HomeFragment:BaseFragment(),HomeContract.View{
     }
 
     override fun setMoreData(itemList: ArrayList<HomeBean.Issue.Item>) {
-
-
+        loadingMore = false
+        mHomeAdapter?.addItemData(itemList)
     }
 
     override fun showError(msg: String, errorCode: Int) {
