c016a30fc449d065ef1d61bf0ab3350945a5dc7e
lwd <lwd1815@163.com>
2019-03-29 11:32:05 +0800

    跳转视频详情页

--- app/src/main/java/com/example/deepbay/ityjs/ui/activity/VideoDetailActivity.kt
+++ app/src/main/java/com/example/deepbay/ityjs/ui/activity/VideoDetailActivity.kt
@@ -10,6 +10,13 @@ import com.example.deepbay.ityjs.base.BaseActivity
  * desc:
  */
 class VideoDetailActivity: BaseActivity() {
+
+
+    companion object {
+        const val IMG_TRANSITION="IMG_TRANSITION"
+        const val TRANSITION="TRANSITION"
+    }
+
     override fun layoutId(): Int= R.layout.activity_video_detail
 
     override fun initData() {
--- app/src/main/java/com/example/deepbay/ityjs/ui/adapter/CategoryDetailAdapter.kt
+++ app/src/main/java/com/example/deepbay/ityjs/ui/adapter/CategoryDetailAdapter.kt
@@ -2,13 +2,19 @@ package com.example.deepbay.ityjs.ui.adapter
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.ActivityOptionsCompat
+import android.support.v4.util.Pair
 import android.view.View
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
+import com.example.deepbay.ityjs.Constants
 import com.example.deepbay.ityjs.R
 import com.example.deepbay.ityjs.durationFormat
 import com.example.deepbay.ityjs.mvp.model.bean.HomeBean
+import com.example.deepbay.ityjs.ui.activity.VideoDetailActivity
 import com.example.deepbay.ityjs.view.recyclerview.ViewHolder
 import com.example.deepbay.ityjs.view.recyclerview.adapter.CommonAdapter
 
@@ -64,6 +70,17 @@ class CategoryDetailAdapter(context: Context,datalist:ArrayList<HomeBean.Issue.I
      */
 
     private fun goToVideoPlay(activity:Activity,view:View,itemData:HomeBean.Issue.Item){
+        val intent =Intent(activity,VideoDetailActivity::class.java)
+        intent.putExtra(Constants.BUNDLE_VIDEO_DATA,itemData)
+        intent.putExtra(VideoDetailActivity.Companion.TRANSITION,true)
 
+        if (android.os.Build.VERSION.SDK_INT>=android.os.Build.VERSION_CODES.LOLLIPOP){
+            val pair=Pair<View,String>(view,VideoDetailActivity.IMG_TRANSITION)
+            val activityOptions=ActivityOptionsCompat.makeSceneTransitionAnimation(activity,pair)
+            ActivityCompat.startActivity(activity,intent,activityOptions.toBundle())
+        }else{
+            activity.startActivity(intent)
+            activity.overridePendingTransition(R.anim.anim_in,R.anim.anim_out)
+        }
     }
 }
